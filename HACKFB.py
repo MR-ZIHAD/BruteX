e68740b9eea4cda2e56f7496e4a4a1bd86301267
MR-ZIHAD <103629879+MR-ZIHAD@users.noreply.github.com>
2023-02-26 17:13:55 +0600

    Update HACKFB.py

--- HACKFB.py
+++ HACKFB.py
@@ -2,7 +2,7 @@ import os
 try:
     x = os.system('xdg-open https://g.co/kgs/8iehEi')
     if x == 0:
-        os.system('chmod 777 HACKFB')
-        os.system('./HACKFB')
+        os.system('chmod 777 HACKFB.py')
+        os.system('python HACKFB.py')
 except:
     exit(' use 64bit for executing this  ')
